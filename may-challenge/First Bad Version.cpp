1ae54eac153984bd0616617e335dacdfb24a507d
Charu Agarwal <charu.agarwal@grabtaxi.com>
2020-05-03 13:01:51 +0530

    May 2

--- may-challenge/First Bad Version.cpp
+++ may-challenge/First Bad Version.cpp
@@ -1,15 +1,18 @@
+// Question:
+// https://leetcode.com/explore/featured/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
+
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
 // Since, the question says you have to optimise the number of calls to isBadVersion API, that is a good enough hint.
 // We know here that if one number is a bad version, all the later ones are too.
 // Two ways to search through this - Linear: O(n) Time Complexity & Binary O(logn) TC
-
+// In linear search, you search through the entire space where as in Binary Search, you keep dividing the search space into half.
 
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 0;s
+        int l = 0;
         int r = n;
         int answer = n;
         while(l<=r){
